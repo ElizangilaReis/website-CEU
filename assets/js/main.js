a427dae42bf9b261396e2f939ff627c6b6b02e87
ElizangilaReis <117958406+ElizangilaReis@users.noreply.github.com>
2023-11-18 15:19:31 +0100

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,13 +117,13 @@
   
   /**
    * Preloader
-   */
+   
   let preloader = select('#preloader');
   if (preloader) {
     window.addEventListener('load', () => {
       preloader.remove()
     });
-  }
+  }*/
 
   // Slider
 
